4879187f4bbb5f86f1fc8e8b00c1654a9c5d47b0
JjackFrost <nongjj_jay@hotmail.com>
2022-09-15 14:49:20 +0700

    finished

--- components/Comment.jsx
+++ components/Comment.jsx
@@ -5,9 +5,36 @@ export default function Comment(props) {
   return (
     <div>
       {/* Entire Comment div */}
-      <div></div>
+      <div>
+        <div className="d-flex gap-2 my-2">
+          <img
+            src={props.data.userImagePath}
+            width="48"
+            height="48"
+            className="rounded-circle"
+            style={{ objectFit: "cover" }}
+          />
+          <div
+            className="rounded rounded-3 p-2"
+            style={{ backgroundColor: "#3A3B3C" }}
+          >
+            <span className="fw-semibold" style={{ color: "#E4E6EB" }}>
+              {props.data.username}
+            </span>
+            <br />
+            <span style={{ color: "#E4E6EB" }}>{props.data.commentText}</span>
+            <div className="d-flex align-items-center gap-1">
+              <img src="/like.svg" width={20}></img>
+              <span className="text-muted">{props.data.likeNum}</span>
+            </div>
+          </div>
+        </div>
+      </div>
 
       {/*render Reply here... */}
+      {props.data.replies.map((element, index) => {
+        return <Reply data={element} key={index} />;
+      })}
     </div>
   );
 }
--- components/PostOwner.jsx
+++ components/PostOwner.jsx
@@ -1,5 +1,33 @@
 import React from "react";
-
+import Comment from "./Comment";
 export default function PostOwner(props) {
-  return <div></div>;
+  return (
+    <div>
+      <div className="vstack gap-3">
+        <div className="d-flex align-items-center gap-3">
+          <img
+            src="/profileImages/Profile.jpg"
+            width="48"
+            height="48"
+            className="rounded-circle"
+            style={{ objectFit: "cover" }}
+          />
+          <span className="fw-semibold fs-5 text-white">
+            Supawit Chaidej 640612194
+          </span>
+        </div>
+        <span className="text-white">
+          Quiz นี้ยากไปไหนครับ ของ่ายกว่านี้ได้ไหม #261207
+        </span>
+        <div className="d-flex align-items-center gap-1">
+          <img src="/like.svg" width={20}></img>
+          <span style={{ color: "#B0B3B8" }}>800 คน</span>
+        </div>
+        <hr className="m-0 border" />
+        {props.comments.map((data, index) => {
+          return <Comment data={data} key={index} />;
+        })}
+      </div>
+    </div>
+  );
 }
